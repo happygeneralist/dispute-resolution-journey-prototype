26cc43c8f5de1eec4384debcdd3a4569237d828c
Ryan <ryan@kwerk.co>
2022-02-18 14:12:52 +0000

    set up main info

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Dispute Resolution Service Journey',
 
   // Default port that prototype runs on
   port: '3000',
